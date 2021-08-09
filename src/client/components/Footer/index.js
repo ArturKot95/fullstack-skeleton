36da9e4cf1bb156bedf4bd5762add41c37a4e586
Artur <arturkot95@gmail.com>
2021-08-09 11:53:43 +0200

    add container to footer

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './footer.css';
 
 export const Footer = () => (
     <Navbar bg="light" expand="xl" className="page-footer">
-        <a href="#" target="_self">Contact us!</a>
+        <Container>
+            <a href="http://arturkot.me" target="_blank">Made by Artur Kot</a>
+        </Container>
     </Navbar>
 );
 
